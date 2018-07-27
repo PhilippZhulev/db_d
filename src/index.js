9c93abf69c9bd990a331e2934b5acf08a292a8fe
PhilippZhulev <filippja@gmail.com>
2018-07-27 13:09:16 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-///sssssss
+///sssssssc
 
 const theme = createMuiTheme({
     palette: {
