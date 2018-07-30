48346c214e85e8bb8602ee50dbc6cd77305e22ec
Anton Kokorev <40538565+antonkokorev@users.noreply.github.com>
2018-07-30 18:03:02 +0300

    Commit

--- src/components/charts/legend.js
+++ src/components/charts/legend.js
@@ -15,7 +15,7 @@ class Legend extends Component {
         const legendItems = this.props.options.colors.map((color,index)=>
             <div key={index} className="legend_item">
                 <div className = "legend_color" style={{backgroundColor: color}}></div>
-                <div className = "legend_text" style={{color: this.props.templ.primary.textValueNormal}}>{this.props.options.titles[index]}</div>
+                <div className = "legend_text" style={{color: this.props.templ.primary.graphText}}>{this.props.options.titles[index]}</div>
             </div>
         );
 
