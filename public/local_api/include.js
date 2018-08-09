247389031f2e8888952f3fcd55202d51cf7210b1
DronVol <dronvol@gmail.com>
2018-08-09 15:46:18 +0300

    1

---
+++ public/local_api/include.js
@@ -0,0 +1,35 @@
+function Reactor() {
+    this.run = function() {
+        //Получаем globalSettings
+        obj.super = {};
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function(){
+            if(xmlHttp.status === 200 && xmlHttp.readyState === 4){
+                obj.super = xmlHttp.responseText;
+            }
+        };
+
+        xmlHttp.open("GET", "local_api/data.json",false);
+        xmlHttp.send();
+
+        //Получаем и парсим JSON
+        obj.changeData = JSON.parse(obj.super);
+        obj.dummyData = JSON.parse(obj.super).data;
+
+        //После парсинга JSON запускаем событие reactRun (Только если payload = START тоесть только при первой загрузке)
+        if(obj.changeData.payload === "START") {
+            document.dispatchEvent(reactRun);
+        }
+
+        //При каждом обновление запукаем событие reactUpdate и даем знать React что загрузка данных завершена
+        document.dispatchEvent(reactUpdate);
+
+    }
+}
+
+setTimeout(function () {
+    const reactor = new Reactor();
+    reactor.run();
+}, 1000);
