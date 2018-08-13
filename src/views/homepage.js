e4be9f248bf6d183bc46ccde51e83f93a14b8e73
unknown <filippja@gmail.com>
2018-08-13 15:53:43 +0300

    1

--- src/views/homepage.js
+++ src/views/homepage.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Tile from './modules/tile';
 import store, {getState, change} from '../reduser';
 
+let data={};
+
+store.subscribe(() => {
+
+    if (change === "first_include") {
+        data = getState.data.data;
+    }
+});
+
 class Home extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: this.props.dataStates.data,
-            funcs: ["PI","INC","OPEX","CIR","KOM","COR","CHIS"],
-            templ: this.props.templ
+            data: data
         };
 
         store.subscribe(() => {
@@ -21,27 +28,29 @@ class Home extends Component {
         });
     }
 
-    tiles = (states) => {
-        return this.state.funcs.map((value,index)=>{
-            const isSmall = (index !== 0);
-            return (
-                <Tile
-                    key = {index}
-                    tileNum = {index + 1}
-                    page = "ALL"
-                    isSmall = {isSmall}
-                    func={value}
-                    templ = {states.templ}
-                    data = {states.data}
-                />
-            )
-        });
-    }; 
-
     render() {
+        const funcs = ["PI","INC","OPEX","CIR","KOM","COR","CHIS"];
+        const templ = this.props.templ;
+        let tiles = funcs.map(
+            (value,index)=>{
+                const isSmall = (index !== 0);
+                return(
+                    <Tile
+                        key={index}
+                        tileNum = {index + 1}
+                        page = "ALL"
+                        isSmall = {isSmall}
+                        func={value}
+                        templ = {templ}
+                        data = {this.state.data}
+                    />
+                )
+            }
+        );
+
         return (
             <div className="tiles_container">
-                {this.tiles(this.state)}
+                {tiles}
             </div>
         )
     }
