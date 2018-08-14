df6f27832e7a3baef012c450a54d7920ab3431ea
DronVol <dronvol@gmail.com>
2018-08-14 14:13:47 +0300

    1

--- src/views/kpi-rb.js
+++ src/views/kpi-rb.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
 import Tile from './modules/tile';
 
-
 class Rb extends Component {
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            data: this.props.fluxData,
+            funcs: ["PI","INC","OPEX","CIR","KOM","COR","CHIS"],
+            templ: this.props.templ
+        };
     }
 
-    render() {
-        const funcs = ["PI","INC","OPEX","CIR","KOM","COR","CHIS"];
-        const templ = this.props.templ;
-        let tiles = funcs.map(
-            (value,index)=>{
-                const isSmall = (index==0) ? false : true;
-                return(
-                    <Tile
-                        key={index}
-                        tileNum = {index + 1}
-                        page = "RB"
-                        addSubscr = "РБ"
-                        isSmall = {isSmall}
-                        func={value}
-                        templ = {templ}
-                    />
+    tiles = () => {
+        return this.state.funcs.map((value,index)=>{
+            const isSmall = (index !== 0);
+            return (
+                <Tile
+                    key = {index}
+                    tileNum = {index + 1}
+                    page = "RB"
+                    isSmall = {isSmall}
+                    func={value}
+                    templ = {this.props.templ}
+                    data = {this.props.fluxData.data}
+                />
             )
-            }
-        );
+        });
+    };
 
+    render() {
         return (
             <div className="tiles_container">
-                {tiles}
+                {this.tiles(this.state)}
             </div>
         )
     }
--- src/views/opexpage.js
+++ src/views/opexpage.js
@@ -1,46 +1,39 @@
 import React, { Component } from 'react';
 import Tile from './modules/tile';
-import store, {change, getState} from "../reduser";
-
-let data={};
-
-store.subscribe(() => {
-
-    if (change === "first_include") {
-        data = getState.data.data;
-    }
-});
 
 class Opex extends Component {
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            data: this.props.fluxData,
+            funcs: ["OPEX_op","PERS_op","IT_op","NEDV_op","BIS_op","MAR_op","PROC_op"],
+            templ: this.props.templ
+        };
     }
 
-    render() {
-        const funcs = ["OPEX_op","PERS_op","IT_op","NEDV_op","BIS_op","MAR_op","PROC_op"];
-        const templ = this.props.templ;
-        let tiles = funcs.map(
-            (value,index)=>{
-                const isSmall = (index !== 0);
-                return(
-                    <Tile
-                        key={index}
-                        tileNum = {index + 1}
-                        grId={index}
-                        tileNum = {index + 1}
-                        page = "OPEX"
-                        isSmall = {isSmall}
-                        func={value}
-                        templ = {templ}
-                        data = {data}
-                    />
+    tiles = () => {
+        return this.state.funcs.map((value,index)=>{
+            const isSmall = (index !== 0);
+            return (
+                <Tile
+                    key = {index}
+                    tileNum = {index + 1}
+                    page = "OPEX"
+                    isSmall = {isSmall}
+                    func={value}
+                    templ = {this.props.templ}
+                    data = {this.props.fluxData.data}
+                />
             )
-            }
-        );
+        });
+    };
 
+    render() {
         return (
             <div className="tiles_container">
-                {tiles}
+                {this.tiles(this.state)}
             </div>
         )
     }
