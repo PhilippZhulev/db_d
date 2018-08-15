2787215635b77e1c264dbf77caf37faf31891557
unknown <filippja@gmail.com>
2018-08-15 10:47:47 +0300

    1

--- src/components/material/slider/SimpleSlider.js
+++ src/components/material/slider/SimpleSlider.js
@@ -10,7 +10,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
 import store from '../../../reduser';
 
 const styles = {
