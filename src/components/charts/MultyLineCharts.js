a69064432928d51c7802d802e4358e1ecb4d4037
Anton Kokorev <40538565+antonkokorev@users.noreply.github.com>
2018-08-26 19:12:37 +0300

    1

--- src/components/charts/MultyLineCharts.js
+++ src/components/charts/MultyLineCharts.js
@@ -87,12 +87,12 @@ class MultiLine extends Component {
             );
 
         }
-        let a = amchartsSettings.graphs[1];
-        let b = amchartsSettings.graphs[2];
-        amchartsSettings.graphs.shift();
-        amchartsSettings.graphs.shift();
-        amchartsSettings.graphs.push(b);
-        amchartsSettings.graphs.push(a);
+        let a = amchartsSettings.graphs[0];
+        let b = amchartsSettings.graphs[1];
+        let c = amchartsSettings.graphs[2];
+        amchartsSettings.graphs[0] = a;
+        amchartsSettings.graphs[1] = b;
+        amchartsSettings.graphs[2] = c;
         console.log("-------------");
         console.log(amchartsSettings);
         console.log("-------------");
