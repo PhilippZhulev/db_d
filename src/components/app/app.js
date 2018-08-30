877a9ada39914a9485a2b1d43d908ef994f65f8a
Anton Kokorev <40538565+antonkokorev@users.noreply.github.com>
2018-08-30 14:19:05 +0300

    1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -240,7 +240,7 @@ class App extends Component {
                     />
                         <Preloader bool={this.state.preloader} />
                     <div className={"app_menu_output" + this.state.menu + this.state.pos} style={{background: this.state.theme.primary.menu}}>
-                        <div style={{height: "82%", margin:"0 -15px", overflowY: "hidden", overflowX: "visible"}}>
+                        <div style={{height: "81%", margin:"0 -15px", overflowY: "hidden", overflowX: "visible"}}>
                             {bar}
                         </div>
                         <div className="btns__panel">
