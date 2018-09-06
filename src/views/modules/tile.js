78e9c32358e5fc2b839f3cab93eefb71d007bd74
unknown <filippja@gmail.com>
2018-09-06 11:14:04 +0300

    1

--- src/views/modules/tile.js
+++ src/views/modules/tile.js
@@ -1,43 +1,75 @@
 import React, { Component } from 'react';
 import MultiLine from '../../components/charts/MultyLineCharts';
 import Model from '../../models/model.js';
+import {withStyles} from "@material-ui/core/styles";
+import Close from '@material-ui/icons/Close';
 
 const tilesBind = Model.tilesBind();
+
+const styles = theme => ({
+  popup : {
+    width: "calc(100% - 250px)",
+    height: "calc(100% - 50px)",
+    position: "fixed",
+    background: "#29353e",
+    zIndex: "9000",
+    top: 50,
+    left: 0
+  },
+  close : {
+    width: 40,
+    height: 40,
+    position: "absolute",
+    right: 15,
+    top: 15
+  },
+  closeIcon: {
+    width: 40,
+    height: 40,
+    fill: "#fff"
+  }
+});
 
 class Tile extends Component {
 
     prec = 0;
 
+    state = {
+      popup: false,
+      popFade: { transform: "scale(0)", opacity: 0, transition: "all 300ms ease-in-out"}
+    };
+
+    componentDidMount = () => {
+        this.setState({popFade: {transform: "scale(1)", opacity: 1, transition: "all 300ms ease-in-out"}})
+    };
+
+    componentWillMount = () => {
+        this.setState({popFade: {transform: "scale(0)", opacity: 0, transition: "all 300ms ease-in-out"}})
+    };
+
+    createPopup = (classes) => {
+      if(this.state.popup === true) {
+        return (
+          <section style={this.state.popFade} className={classes.popup}>
+              <div className={classes.close}>
+                <Close onClick={this.closePopup} className={classes.closeIcon} />
+              </div>
+          </section>
+        );
+      }else {
+        return <section style={this.state.popFade} />
+      }
+    };
+
+    openPopup = () => {
+      this.setState({popup: true});
+    };
+
+    closePopup = () => {
+      this.setState({popup: false});
+    };
+
     setValue = (postfix, subscribtion, color) => {
-        // let values = [],
-        //     tileCalc = Model.tileCalc(this.props.func, this.props.page, this.props.data, this.props.date, tilesBind[this.props.func].mera);
-        //
-        // let zeroedVal = null;
-        // let zeroedSmallVal = null;
-        //
-        // if ((this.props.func === "EPS") || (this.props.func === "COR")) {
-        //     zeroedVal = tileCalc.mainVal.split(".");
-        //     if ((zeroedVal.length > 1) && (zeroedVal[1].length < 2)) {
-        //         zeroedVal[1] = zeroedVal[1] + "0";
-        //         zeroedVal = zeroedVal.join(".");
-        //     } else if (zeroedVal.length === 1) {
-        //         zeroedVal = [tileCalc.mainVal, "00"].join(".");
-        //     } else if ((zeroedVal.length > 1) && (zeroedVal[1].length > 2)) {
-        //         zeroedVal[1] = zeroedVal[1].substr(0, 2);
-        //         zeroedVal = zeroedVal.join(".");
-        //     } else{
-        //         zeroedVal = null;
-        //     }
-        // } else if(((this.props.func === "OPEX")&&(this.props.page === "ALL"))||((this.props.func === "CHIS")&&(this.props.page === "ALL"))){
-        //     let zeroedVal = null;
-        // } else{
-        //     zeroedVal = tileCalc.mainVal.split(".");
-        //     if (zeroedVal.length === 1){
-        //         zeroedVal = [tileCalc.mainVal,"0"].join(".");
-        //     } else{
-        //         zeroedVal = tileCalc.mainVal;
-        //     }
-        // }
         let values = [];
         let prec = 1;
         if ((this.props.func === "EPS") || (this.props.func === "COR")) {
@@ -57,9 +89,6 @@ class Tile extends Component {
         }
         this.graphcolor = graphcolor;
         this.label = label;
-
-
-        //console.log(prec);
 
         let tileCalc = Model.tileCalc(this.props.func, this.props.page, this.props.data, this.props.date, tilesBind[this.props.func].mera, prec);
 
@@ -87,10 +116,12 @@ class Tile extends Component {
         let subscribtion = "1";
         let color = "rgba(0,0,0,0)";
 
+        const { classes } = this.props;
+
         return(
             <div className={"tile_item_"+this.props.tileNum}>
                 <div className="tile_item__inner" style={{background: this.props.templ.primary.tiles}}>
-                    <div className={"tile_item__title"+postfix} style={{color: this.props.templ.primary.textValueMain}}>
+                    <div onClick={this.openPopup} className={"tile_item__title"+postfix} style={{color: this.props.templ.primary.textValueMain}}>
                         {tilesBind[this.props.func].title}<span style={{color: this.props.templ.primary.textValueNormal}}>{((tilesBind[this.props.func].mera === "") ? "" : ", ")+tilesBind[this.props.func].mera}</span>
                     </div>
                     <div className="values_wrapper main_tile">
@@ -118,9 +149,10 @@ class Tile extends Component {
                         prec={this.prec}
                     />
                 </div>
+                {this.createPopup(classes)}
             </div>
         )
     }
 }
 
-export default Tile;
+export default withStyles(styles)(Tile);
